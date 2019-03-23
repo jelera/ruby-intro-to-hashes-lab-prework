c34d6448d21910e6c5e8dbb1a751af3277740ed3
Jose Elera <jelera@gmail.com>
2019-03-23 03:09:02 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ end
 def monopoly_with_second_tier
 	#copy and paste the jmonopoly hash you built in the previous method. We're going to add stuff to it here!
 	monopoly_with_second_tier = {
-		:railroads = {
+		:railroads => {
 			:pieces => 4,
 			:names => {},
 			:rent_in_dollars => {}
